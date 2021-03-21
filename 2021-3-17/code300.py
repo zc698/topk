a87a3158373ea86f934545e9bb5937e18cf614bc
zc698 <961885570@qq.com>
2021-03-21 18:04:05 +0800

    修改

---
+++ 2021-3-17/code300.py
@@ -0,0 +1,62 @@
+"""
+300.最长递增子序列
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+
+子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+示例1：
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+"""
+
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+        
+        
+"""
+时间复杂度：O(n^2),其中n为数组nums的长度。
+空间复杂度:O(n)，需要额外使用长度为n的dp数组。
+"""
+
+"""
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        d = []
+        for n in nums:
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                l, r = 0, len(d) - 1
+                loc = r
+                while l <= r:
+                    mid = (l + r) // 2
+                    if d[mid] >= n:
+                        loc = mid
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                d[loc] = n
+        return len(d)
+
+
+时间复杂度：O(nlogn)。数组nums 的长度为n，我们依次用数组中的元素去更新d数组，而更新d数组时需要进行O(logn) 的二分搜索，所以总时间复杂度为 O(nlogn)。
+空间复杂度：O(n)，需要额外使用长度为n 的d 数组。
+"""
