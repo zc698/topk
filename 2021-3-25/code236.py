04e1470298842a6887113adb4cbcba50e29d98bc
zc698 <961885570@qq.com>
2021-03-25 23:32:41 +0800

    打卡

---
+++ 2021-3-25/code236.py
@@ -0,0 +1,41 @@
+236.二叉树的最近公共祖先
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。
+
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出：3
+解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+
+2021.3.25
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+思路：
+通过递归对二叉树进行后序遍历，当越过叶节点，返回null,当root等于p,q返回root.
+
+时间复杂度：O(n),n为二叉树的节点数。
+空间复杂度：O(n)，n为二叉树的节点数。
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if not left and not right:
+            return 
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
